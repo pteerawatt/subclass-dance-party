f4e9631df3268d0aab2271063d487b9608d702a1
pteerawatt <p.teerawatt@gmail.com>
2020-02-10 19:06:15 -0800

    got initial tests to pass. blinky  dancer working

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -1,11 +1,11 @@
 describe('blinkyDancer', function() {
-
+  // debugger;
   var blinkyDancer, clock;
   var timeBetweenSteps = 100;
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    blinkyDancer = new makeBlinkyDancer(10, 20, timeBetweenSteps);
   });
 
   it('should have a jQuery $node object', function() {
@@ -14,7 +14,7 @@ describe('blinkyDancer', function() {
 
   it('should have a step function that makes its node blink', function() {
     sinon.spy(blinkyDancer.$node, 'toggle');
-    blinkyDancer.step();
+    clock.tick(timeBetweenSteps);
     expect(blinkyDancer.$node.toggle.called).to.be.true;
   });
 
